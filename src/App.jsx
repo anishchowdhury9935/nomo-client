d6837c5dbd8e8602434d41e12c9170b8157df15e
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-06-16 13:40:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
 import './App.css'
 import Home from './components/Home';
 import Navbar from './components/Navbar';
@@ -11,17 +6,13 @@ function App() {
   return (
     <div className='main_site'>
       <title>NOMO | Not Missing Out</title>
-      <Router>
-        <Modal />
-        <div className='main_site_nav'>
-          <Navbar />
-        </div>
-        <div className='main_site_content'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-          </Routes>
-        </div>
-      </Router>
+      <Modal />
+      <div className='main_site_nav'>
+        <Navbar />
+      </div>
+      <div className='main_site_content'>
+        <Home />
+      </div>
     </div>
   )
 }
